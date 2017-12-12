ea9a4e1d8c7d1c530a36c391cbbd608a560cb290
Mario Nebl <hello@mario-nebl.de>
2017-12-12 22:36:35 +0100

    Improve test cases

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,26 @@ pub fn reversor(input: &str) -> String {
 }
 
 #[test]
+fn test_empty() {
+    assert_eq!(reversor(""), "");
+}
+
+#[test]
+fn test_single() {
+    assert_eq!(reversor("a"), "a");
+}
+
+#[test]
+fn test_single_utf8() {
+    assert_eq!(reversor("😁"), "😁");
+}
+
+#[test]
 fn test_hello() {
     assert_eq!(reversor("hello"), "olleh");
+}
+
+#[test]
+fn test_utf8() {
+    assert_eq!(reversor("😁😂"), "😂😁");
 }
