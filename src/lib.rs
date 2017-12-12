f53efe5f20bd4ccc87e28a646dc5a8ff69529c74
Mario Nebl <hello@mario-nebl.de>
2017-12-12 23:06:43 +0100

    Wrap tests into idiomatic mod

--- src/lib.rs
+++ src/lib.rs
@@ -2,27 +2,32 @@ pub fn reversor(input: &str) -> String {
     input.chars().rev().collect()
 }
 
-#[test]
-fn test_empty() {
-    assert_eq!(reversor(""), "");
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[test]
-fn test_single() {
-    assert_eq!(reversor("a"), "a");
-}
+    #[test]
+    fn test_empty() {
+        assert_eq!(reversor(""), "");
+    }
 
-#[test]
-fn test_single_utf8() {
-    assert_eq!(reversor("😁"), "😁");
-}
+    #[test]
+    fn test_single() {
+        assert_eq!(reversor("a"), "a");
+    }
 
-#[test]
-fn test_hello() {
-    assert_eq!(reversor("hello"), "olleh");
-}
+    #[test]
+    fn test_single_utf8() {
+        assert_eq!(reversor("😁"), "😁");
+    }
 
-#[test]
-fn test_utf8() {
-    assert_eq!(reversor("😁😂"), "😂😁");
+    #[test]
+    fn test_hello() {
+        assert_eq!(reversor("hello"), "olleh");
+    }
+
+    #[test]
+    fn test_utf8() {
+        assert_eq!(reversor("😁😂"), "😂😁");
+    }
 }
